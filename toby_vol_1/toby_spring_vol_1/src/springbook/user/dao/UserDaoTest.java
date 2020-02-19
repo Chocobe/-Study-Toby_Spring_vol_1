bb841a67fe574a739af056033e73731e94c972c4
Chocobe <nice_10bird@naver.com>
2020-02-19 14:59:27 +0900

    <master> : Tested "SingleTon Registry"

--- toby_vol_1/toby_spring_vol_1/src/springbook/user/dao/UserDaoTest.java
+++ toby_vol_1/toby_spring_vol_1/src/springbook/user/dao/UserDaoTest.java
@@ -9,6 +9,14 @@ import springbook.user.domain.User;
 
 public class UserDaoTest {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		testEqualsByDaoFactory();
+		
+		testEqualsByApplicationContext();
+	}
+	
+	
+// 1. 스프링 컨테이너(애플리케이션 컨텍스트) 테스트
+	public static void testIoC() throws ClassNotFoundException, SQLException {
 		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
 		UserDao dao = context.getBean("userDao", UserDao.class);
 		
@@ -26,5 +34,27 @@ public class UserDaoTest {
 		System.out.println("ID : " + user_result.getId());
 		System.out.println("Name : " + user_result.getName());
 		System.out.println("Password : " + user_result.getPassword());
+	}
+	
+	
+// 2. getBean()이 반환하는 오브젝트의 동일성 테스트
+	public static void testEqualsByDaoFactory() {
+		DaoFactory factory = new DaoFactory();
+		
+		UserDao dao_1 = factory.userDao();
+		UserDao dao_2 = factory.userDao();
+		
+		System.out.println("factory.userDao() 첫번쨰 : " + dao_1);
+		System.out.println("factory.userDao() 두번째 : " + dao_2);
+	}
+	
+	public static void testEqualsByApplicationContext() {
+		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		
+		UserDao dao_1 = context.getBean("userDao", UserDao.class);
+		UserDao dao_2 = context.getBean("userDao", UserDao.class);
+		
+		System.out.println("ApplicationContext.getBean() 첫번째 : " + dao_1);
+		System.out.println("ApplicationContext.getBean() 두번째 : " + dao_2);
 	}
 }
