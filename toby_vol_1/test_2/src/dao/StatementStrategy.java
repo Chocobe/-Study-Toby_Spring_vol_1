43816188d05cb5da21da53f3cc988ed5e762b031
Chocobe <nice_10bird@naver.com>
2020-02-25 13:47:28 +0900

    <master> : Removed test project

--- toby_vol_1/test_2/src/dao/StatementStrategy.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-public interface StatementStrategy {
-	abstract public PreparedStatement makePreparedStatement(Connection conn) throws SQLException;
-}
